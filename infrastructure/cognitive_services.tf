ce45a0eb38a3d604ecc3e42c46002a33a90c27e8
Mark <mark.henricks@live.com>
2024-02-11 21:51:48 -0600

    ci: cognitive fix

--- infrastructure/cognitive_services.tf
+++ infrastructure/cognitive_services.tf
@@ -1,11 +1,11 @@
-resource "azurerm_cognitive_account" "this" {
-  name                = local.cognitive_account_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  kind                = "ComputerVision"
-  sku_name            = "S0"
+# resource "azurerm_cognitive_account" "this" {
+#   name                = local.cognitive_account_name
+#   resource_group_name = azurerm_resource_group.this.name
+#   location            = azurerm_resource_group.this.location
+#   kind                = "ComputerVision"
+#   sku_name            = "S0"
 
-  tags = {
-    Acceptance = "Test"
-  }
-}
+#   tags = {
+#     Acceptance = "Test"
+#   }
+# }
